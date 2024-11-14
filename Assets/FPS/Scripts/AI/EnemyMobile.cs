01065fbfe4e8950493b9ea716bb7426ece276bf5
c55702980 <c55702980@naver.com>
2024-11-14 17:55:39 +0900

    1. Enemy 패트롤 구현

---
+++ Assets/FPS/Scripts/AI/EnemyMobile.cs
@@ -0,0 +1,80 @@
+using Unity.FPS.Game;
+using UnityEngine;
+
+namespace Unity.FPS.AI
+{
+    /// <summary>
+    /// Enemy 상태
+    /// </summary>
+    public enum AIState
+    {
+        Patrol,
+        Follow,
+        Attack
+    }
+    /// <summary>
+    /// 이동하는 Enemy의 상태들을 구현하는 클래스
+    /// </summary>
+    public class EnemyMobile : MonoBehaviour
+    {
+        #region Variables
+        public Animator animator;
+        private EnemyController enemyController;
+        public AIState AiState {  get; private set; }
+
+        //Sfx
+        public AudioClip movementSound;
+        public MinMaxFloat pitchMovementSpeed;
+        
+        private AudioSource audioSource;
+
+        //animation parameter
+        const string k_AnimAttackParameter = "Attack";
+        const string k_AnimMoveSpeedParameter = "MoveSpeed";
+        const string k_AnimAlertedParameter = "Alerted";
+        const string k_AnimOnDamagedParameter = "OnDamaged";
+        const string k_AnimDeathdParameter = "Death";
+        #endregion
+
+        private void Start()
+        {
+            //참조
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = movementSound;
+            audioSource.Play();
+
+            enemyController = GetComponent<EnemyController>();
+            enemyController.Damaged += OnDamaged;
+
+            //초기화
+            AiState = AIState.Patrol;
+        }
+        private void Update()
+        {
+            UpdateCurrentAiState();
+        }
+        //상태에 따른 Enemy 구현
+        private void UpdateCurrentAiState()
+        {
+            switch (AiState)
+            {
+                case AIState.Patrol:
+                    //이동
+                    enemyController.UpdatePathDestination(true);
+                    enemyController.SetNavDestination(enemyController.GetDestinationOnpath());
+                    break;
+                case AIState.Follow:
+                    //추적
+                    break;
+                case AIState.Attack:
+                    //공격
+                    break;
+            }
+        }
+
+        void OnDamaged()
+        {
+
+        }
+    }
+}
